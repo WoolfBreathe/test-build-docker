98b4968e936736ccced29973f5ae5e5bdf8a6d4c
root <s365548382@sina.com>
2022-10-24 12:58:25 +0000

    tekton_git-init:v0.40.2

--- pull.sh
+++ pull.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-IMAGE_CTL="crictl -n k8s.io"
+IMAGE_CTL="/usr/bin/ctr -n k8s.io"
 
 source env.sh
 
 source ${IMAGE_ENV_PATH}/${IMAGE_ENV_NAME}/params.sh
 
-$("${IMAGE_CTL} image pull ${IMAGE_TO}")
+bash -c "${IMAGE_CTL} image pull ${IMAGE_TO}"
 
-$("${IMAGE_CTL} image tag ${IMAGE_TO} ${IMAGE_FROM}")
+bash -c "${IMAGE_CTL} image tag ${IMAGE_TO} ${IMAGE_FROM}"
+
