aa8c4690996cd5fc1d1c76819050845140452d12
root <s365548382@sina.com>
2022-10-25 13:32:51 +0000

    knative_operator:v1.8.0

---
+++ images/knative/opeartor/params.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+export IMAGE_FROM="gcr.io/knative-releases/knative.dev/operator/cmd/operator@sha256:7df5f59ebcc5e8a1b67814e3b6301a23f6553d9b84446552372a9cb54bad018d"
+
+export IMAGE_TO="docker.io/365548382/knative_operator:v1.8.0"
