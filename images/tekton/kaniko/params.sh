784b156801cb187fc7e8dfee12581951e6afae2e
root <s365548382@sina.com>
2022-10-26 02:04:38 +0000

    tekton_kaniko-executor:v1.5.1

---
+++ images/tekton/kaniko/params.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+export IMAGE_FROM="gcr.io/kaniko-project/executor:v1.5.1@sha256:c6166717f7fe0b7da44908c986137ecfeab21f31ec3992f6e128fff8a94be8a5"
+
+export IMAGE_TO="docker.io/365548382/tekton_kaniko-executor:v1.5.1"
