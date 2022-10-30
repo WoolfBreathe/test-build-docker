6ed70872b77a73237b47c1c249a0f22982107966
root <s365548382@sina.com>
2022-10-30 10:32:49 +0000

    tekton_workingdirinit:v20221030

--- env.sh
+++ env.sh
@@ -2,9 +2,5 @@
 
 export IMAGE_ENV_PATH="images"
 
-<<<<<<< HEAD
 export IMAGE_ENV_NAME="tekton/workingdirinit"
-=======
-export IMAGE_ENV_NAME="ingress-nginx/controller"
->>>>>>> 017e0338c8292f676c681e01fcd35f9c759c2231
 
